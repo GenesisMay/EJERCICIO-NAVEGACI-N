d546ba0fb807e5d4e0aef612d9ec13d088805abb
GenesisMay <genesisdayanamayanzatonato@gmail.com>
2024-08-06 20:23:04 -0500

    ejercicio

--- src/components/InPutComponent.tsx
+++ src/components/InPutComponent.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextInput, TextInputProps } from 'react-native';
+import { TextInput} from 'react-native';
 import { styles } from '../theme/appTheme';
 
 interface Props {
