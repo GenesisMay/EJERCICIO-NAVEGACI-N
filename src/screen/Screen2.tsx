6a1110e71a589a6a4d7bcfc95c15bb34a8de31fc
GenesisMay <genesisdayanamayanzatonato@gmail.com>
2024-08-06 20:21:45 -0500

    ejercicio

--- src/screen/Screen2.tsx
+++ src/screen/Screen2.tsx
@@ -14,7 +14,7 @@ export const Screen2 = () => {
       const num2 = Number(numero2);
 
       const respuesta = 
-      num1 === 0 && num2 === 0 ? 'INDETERMINÁDO':
+      num1 === 0 && num2 === 0 ? 'INDETERMINACÍON':
       num2 === 0 ? 'NO EXISTE DIVISIÓN PARA CERO':
       (num1/num2).toString();
 
@@ -23,14 +23,14 @@ export const Screen2 = () => {
 
   return (
     <View style={styles.container2}>
-      <Text style={styles.text}>Campo numero 1</Text>
+      <Text style={styles.text}>Campo número 1</Text>
       <InputComponent
       placeholder='Ingrese el número'
       value={numero1}
       onChangeText={setNumero1}
       />
 
-      <Text style={styles.text}>Campo numero 2</Text>
+      <Text style={styles.text}>Campo número 2</Text>
       <InputComponent
       placeholder='Ingrese el número'
       value={numero2}
